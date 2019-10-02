cc4d42ff4a522267dd8e2043f5b01040208fb5b8
megatimes <jonathanbrockman@gmail.com>
2019-10-02 18:23:53 -0400

    reinstalled netlify CMS gatsby plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,8 +51,8 @@ const IndexPage = ({
       <Hero>
         <h1>Hi.</h1>
         <p>
-          I&apos;m Jonathan Brockman, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          I&apos;m Jonathan Brockman, a Senior UX Developer with five years of industry experience, specializing in
+          developing React apps with the best UX users can get.
         </p>
         <Link to="/contact">
           <Button big>
